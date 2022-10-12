a7fb6867e0ca1ab3dd6233788a70b139e0381f30
Ederson Silva <edersigner@gmail.com>
2022-10-12 20:43:58 -0300

    Swagger
    Implementando o swagger

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,8 @@
+import swaggerui from "swagger-ui-express";
+
 import { app } from ".";
+import swaggerFile from "../swagger.json";
+
+app.use("/api-docs", swaggerui.serve, swaggerui.setup(swaggerFile));
 
 app.listen(3333, () => console.log("Server is running!"));
